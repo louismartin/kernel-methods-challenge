6ed2e5ccec20b852c884d321221d2fbfe1115bc8
Louis MARTIN <louisrtm@gmail.com>
2017-02-22 10:08:33 +0100

    Add load_images method

---
+++ demo.py
@@ -0,0 +1,7 @@
+from src.utils import DATA_DIR
+from src.data_processing import load_images
+
+Xtr_path = os.path.join(DATA_DIR, "Xtr.csv")
+Xtr = load_images(Xtr_path)
+
+print("Loaded images of shape {}".format(Xtr.shape))
