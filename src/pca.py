90dbdf49509e45fec666a9c3406dda49ef5b757a
BERRADA Karim <berrada.ahmed.karim@gmail.com>
2017-03-02 18:31:46 +0100

    changing names

--- src/pca.py
+++ src/pca.py
@@ -1,15 +1,15 @@
 import numpy as np
 
 
-class pca_:
+class PCA:
 
     def __init__(self, n_components):
 
         self.n_components = n_components
-        self.explained_variance_ratio_ = None
-        self.explained_variance_ = None
+        self.e_values_ = None
+        self.e_values_ratio_ = None
 
-    def fit_transform(self, X, scale=True):
+    def fit(self, X, scale=True):
 
         n_samples, n_features = X.shape
 
@@ -22,10 +22,10 @@ class pca_:
         _e_vectors = u[:, :self.n_components]
         _e_vectors *= s[:self.n_components]
 
-        explained_variance_ = (s ** 2) / n_samples
-        self.explained_variance_ = explained_variance_[:self.n_components]
+        e_values_ = (s ** 2) / n_samples
+        self.e_values_ = e_values_[:self.n_components]
 
-        total_var = float(explained_variance_.sum())
-        self.explained_variance_ratio_ = [float(e) / total_var for e in self.explained_variance_]
+        total_var = float(e_values_.sum())
+        self.e_values_ratio_ = [float(e) / total_var for e in self.e_values_]
 
         return _e_vectors
