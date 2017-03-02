1dceaedbe63cead15c24b64dcb86fc7744aeec54
BERRADA Karim <berrada.ahmed.karim@gmail.com>
2017-03-02 14:58:51 +0100

    augmente data

--- demo.py
+++ demo.py
@@ -4,7 +4,7 @@ import pandas as pd
 import numpy as np
 
 from src.utils import DATA_DIR
-from src.data_processing import load_images, plot_image
+from src.data_processing import load_images, plot_image, vec2img, img2vec, transform_T
 
 
 Xtr_path = os.path.join(DATA_DIR, "Xtr.csv")
@@ -18,3 +18,10 @@ Ytr = np.array(Ytr_csv.tolist())
 print("Loaded images of shape {}".format(Xtr.shape))
 
 plot_image(Xtr[1])
+
+data_augmente = True
+if data_augmente:
+
+    Xtr_reshaped = vec2img(Xtr)
+    tf_Xtr_reshaped, tf_Ytr = transform_T(Xtr_reshaped, Ytr)
+    Xtr = img2vec(Xtr_reshaped)
