da334cc320eabf36119fb1f24ccacab65b20f535
Louis MARTIN <louisrtm@gmail.com>
2017-03-03 15:14:04 +0000

    Clean demo.py

--- demo.py
+++ demo.py
@@ -1,12 +1,17 @@
 import os
+import time
 
 import pandas as pd
 import numpy as np
 from sklearn.metrics import accuracy_score
+from sklearn.multiclass import OneVsRestClassifier
+from sklearn import svm
+from sklearn.decomposition import PCA as PCAsklearn
 
 from src.utils import DATA_DIR
 from src.data_processing import load_images, plot_image, vec2img, img2vec, transform_T
 from src.models import KernelSVM
+from src.pca import PCA
 
 
 Xtr_path = os.path.join(DATA_DIR, "Xtr.csv")
@@ -20,13 +25,26 @@ Ytr = np.array(Ytr_csv.tolist())
 print("Loaded images - shape {}".format(Xtr.shape))
 
 
-data_augment = True
+data_augment = False
 if data_augment:
-
     Xtr_reshaped = vec2img(Xtr)
     tf_Xtr_reshaped, tf_Ytr = transform_T(Xtr_reshaped, Ytr)
     Xtr = img2vec(tf_Xtr_reshaped)
     Ytr = tf_Ytr
+
+# PCA
+do_pca = True
+sklearn_pca = False
+if do_pca:
+    tic = time.time()
+    print("Applying PCA")
+    if sklearn_pca:
+        pca = PCAsklearn(n_components=100)
+        Xtr = pca.fit_transform(Xtr)
+    else:
+        pca = PCA(n_components=100)
+        Xtr = pca.fit(Xtr, scale=False)
+    print("PCA applied in {0:.1f}s".format(time.time() - tic))
 
 # Train / validation split
 train_ratio = 0.8
@@ -37,10 +55,20 @@ Yval = Ytr[n_train_samples:]
 Xtr = Xtr[:n_train_samples]
 Ytr = Ytr[:n_train_samples]
 
-svm = KernelSVM(C=1, kernel='linear')
-svm.train(Xtr, Ytr)
+# Training
+print("Start training")
+sklearn_svm = True
+tic = time.time()
+if sklearn_svm:
+    model = OneVsRestClassifier(svm.SVC(C=1., kernel='rbf', gamma=0.1))
+    model.fit(Xtr, Ytr)
+else:
+    model = KernelSVM(C=1, kernel='linear')
+    model.train(Xtr, Ytr)
+print("Model trained in {0:.1f}s".format(time.time() - tic))
 
-Ypred = svm.predict(Xval)
+Ypred = model.predict(Xval)
+
 
 accuracy = accuracy_score(Yval, Ypred)
-print(accuracy)
+print("Accuracy: {}".format(accuracy))
