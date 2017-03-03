dfc4d37f878e69045ab30177f9e66de47e4a21d8
Louis MARTIN <louisrtm@gmail.com>
2017-03-03 13:51:29 +0000

    Update demo.py with Pierre's SVM

--- demo.py
+++ demo.py
@@ -2,9 +2,11 @@ import os
 
 import pandas as pd
 import numpy as np
+from sklearn.metrics import accuracy_score
 
 from src.utils import DATA_DIR
 from src.data_processing import load_images, plot_image, vec2img, img2vec, transform_T
+from src.models import KernelSVM
 
 
 Xtr_path = os.path.join(DATA_DIR, "Xtr.csv")
@@ -15,15 +17,30 @@ Ytr_csv = pd.read_csv(Ytr_path).Prediction
 Ytr_unique = Ytr_csv.unique()
 Ytr = np.array(Ytr_csv.tolist())
 
-print("Loaded images of shape {}".format(Xtr.shape))
+print("Loaded images - shape {}".format(Xtr.shape))
 
-red_car_index = 3132
-plot_image(Xtr[red_car_index])
 
-data_augmente = True
-if data_augmente:
+data_augment = True
+if data_augment:
 
     Xtr_reshaped = vec2img(Xtr)
     tf_Xtr_reshaped, tf_Ytr = transform_T(Xtr_reshaped, Ytr)
     Xtr = img2vec(tf_Xtr_reshaped)
     Ytr = tf_Ytr
+
+# Train / validation split
+train_ratio = 0.8
+n_samples = Xtr.shape[0]
+n_train_samples = int(n_samples * train_ratio)
+Xval = Xtr[n_train_samples:]
+Yval = Ytr[n_train_samples:]
+Xtr = Xtr[:n_train_samples]
+Ytr = Ytr[:n_train_samples]
+
+svm = KernelSVM(C=1, kernel='linear')
+svm.train(Xtr, Ytr)
+
+Ypred = svm.predict(Xval)
+
+accuracy = accuracy_score(Yval, Ypred)
+print(accuracy)
